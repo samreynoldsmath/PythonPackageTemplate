0e3af1a4f07cd19e1e32a6406cdf6333029290d1
Sam Reynolds <sreyn@proton.me>
2024-03-19 18:10:02 -0700

    add docstring to init

--- example_package_samreynoldsmath/__init__.py
+++ example_package_samreynoldsmath/__init__.py
@@ -1,3 +1,12 @@
+"""
+example_package_samreynoldsmath
+===============================
+
+This is a simple example package that contains a single class, `Solver`, which
+solves a linear system of equations. The package also contains a test suite
+that can be run with `pytest`.
+"""
+
 from .solver import Solver
 
 __all__ = ["Solver"]
